3d47dbde42d58bcc354d23cf4a8aff431ad029aa
diskon-member <stokmatzevo@gmail.com>
2026-08-06 00:13:00 +0700

    Add SettingActivity class for settings screen

--- app/src/main/java/com/paragon/control/SettingActivity.java
+++ app/src/main/java/com/paragon/control/SettingActivity.java
@@ -1,1 +1,14 @@
+package com.paragon.control;
 
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
